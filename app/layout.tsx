dd559de5367a88d906ee68a5a197dccfb626c060
gregorio_ospina <gregorio.ospina@rappi.com>
2024-08-26 11:21:30 -0500

    viewPort

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,14 +1,17 @@
-import type { Metadata } from "next";
+import type { Metadata, Viewport } from "next";
 import { Open_Sans, Playfair_Display } from "next/font/google";
 import "./globals.css";
 
 const playfair = Playfair_Display({ subsets: ["latin"], variable: "--playfair" });
 const opensans = Open_Sans({ subsets: ["latin"], variable: "--opensans" });
 
+export const viewport: Viewport = {
+	themeColor: "#000000",
+};
+
 export const metadata: Metadata = {
 	title: "Tatiana Voronetska",
 	description: "Aprender no tiene límites",
-	themeColor: "#000000",
 };
 
 export default function RootLayout({
