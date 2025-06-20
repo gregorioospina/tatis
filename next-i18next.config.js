6336ec400994ebc157dead85978fd69031417d21
gregorio_ospina <gregorio.ospina@rappi.com>
2025-06-20 11:28:19 -0500

    va

--- next-i18next.config.js
+++ next-i18next.config.js
@@ -2,7 +2,6 @@ module.exports = {
 	i18n: {
 		defaultLocale: "ru",
 		locales: ["es", "en", "ru"],
-		localeDetection: false,
 	},
 	localePath: "./locales",
 	reloadOnPrerender: process.env.NODE_ENV === "development",
